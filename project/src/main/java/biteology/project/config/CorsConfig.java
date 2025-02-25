eecb7e799bcd6689d22a80b03ec6af6978a0ce05
TriNhan <trinhan452003@gmail.com>
2025-02-25 10:01:57 +0700

    test - fix cors

--- project/src/main/java/biteology/project/config/CorsConfig.java
+++ project/src/main/java/biteology/project/config/CorsConfig.java
@@ -1,16 +1,27 @@
 package biteology.project.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Cho phép tất cả API
+                        .allowedOrigins("*") // Cho phép mọi domain gọi API
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Cho phép các method API
+                        .allowedHeaders("*") // Cho phép tất cả header
+                        .allowCredentials(false);
+            }
+        };
     }
 }
